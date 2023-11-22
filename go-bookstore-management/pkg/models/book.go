1b19a69f71d67089cb4d2ff4255f467d2d148879
Sufiyan33 <basu700736@gmail.com>
2023-11-22 19:17:00 +0530

    Added method which will called by controllers

--- go-bookstore-management/pkg/models/book.go
+++ go-bookstore-management/pkg/models/book.go
@@ -21,3 +21,27 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBook() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookByID(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
+}
