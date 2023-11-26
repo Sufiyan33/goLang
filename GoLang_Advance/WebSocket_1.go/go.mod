01f611a0984a94fb74ab30e467d9c6a434142f82
Sufiyan33 <basu700736@gmail.com>
2023-11-26 20:12:58 +0530

    Developing socket

---
+++ GoLang_Advance/WebSocket_1.go/go.mod
@@ -0,0 +1,10 @@
+module github.com/Sufiyan33/goLang/tree/main/GoLang_Advance/WebSocket_1.go
+
+go 1.21.4
+
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/googollee/go-socket.io v1.7.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
--- GoLang_Advance/WebSocket_1.go/main.go
+++ GoLang_Advance/WebSocket_1.go/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
 
 func main() {
 	fmt.Println("Hello websocket")
+
+	server := socketio.NewServer(nil)
+
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+
+		s.join("chat")
+		
+	})
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	http.Handle("/socket.io/", server)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
 }
