70a3594d4bd5fd6e7ba81654569e4bc6801441c0
Sufiyan33 <basu700736@gmail.com>
2023-11-26 16:38:29 +0530

    Made some changes

--- GoLang Advance/RestApiWithGORM/user.go
+++ GoLang Advance/RestApiWithGORM/user.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 )
 
@@ -43,12 +45,26 @@ func NewUser(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	var users []User
-	db.Find(&users)
+	params := mux.Vars(r)
+	name := params["name"]
+	email := params["email"]
+	db.Create(&User{Name: name, Email: email})
+	fmt.Fprintf(w, "Record succesfully created")
 }
 
 func AllUsers(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Fetching all user with GET method")
+
+	db, err := sql.Open("mysql", "root:Mysql@@123@tcp(127.0.0.1:3306)/sufiyan")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect databse")
+	}
+	defer db.Close()
+
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(users)
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
