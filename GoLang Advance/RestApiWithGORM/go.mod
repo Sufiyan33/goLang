2cd5215f1b4cf29e2a5f51d599608d9fa3eed03a
Sufiyan33 <basu700736@gmail.com>
2023-11-26 17:17:18 +0530

    Creating Rest api with GORM

---
+++ GoLang Advance/RestApiWithGORM/go.mod
@@ -0,0 +1,13 @@
+module github.com/Sufiyan33/goLang/tree/main/GoLangAdvance/RestApiWithGORM
+
+go 1.21.4
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/mysql v1.5.2 // indirect
+	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55 // indirect
+)
---
+++ GoLang Advance/RestApiWithGORM/user.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
+
+func InitialiMigration() {
+	db, err := sql.Open("mysql", "root:Mysql@@123@tcp(127.0.0.1:3306)/sufiyan")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect databse")
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+	db.AutoMigrate(&User{})
+}
+
+func NewUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Creating new user with POST method")
+
+	db, err := sql.Open("mysql", "root:Mysql@@123@tcp(127.0.0.1:3306)/sufiyan")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect databse")
+	}
+	defer db.Close()
+
+	params := mux.Vars(r)
+	name := params["name"]
+	email := params["email"]
+	db.Create(&User{Name: name, Email: email})
+	fmt.Fprintf(w, "Record succesfully created")
+}
+
+func AllUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Fetching all user with GET method")
+
+	db, err := sql.Open("mysql", "root:Mysql@@123@tcp(127.0.0.1:3306)/sufiyan")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect databse")
+	}
+	defer db.Close()
+
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Updating user details with PUT method")
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Deleting user details with DELTE method")
+}
